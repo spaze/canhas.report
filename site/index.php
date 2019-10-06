c4d0d7cbeb57db6f14a844199e4e8ef92fe3acaf
Michal Špaček <mail@michalspacek.cz>
2019-10-06 13:08:09 +0200

    Redirect reporting subdomain root to base domain

--- site/index.php
+++ site/index.php
@@ -1,5 +1,9 @@
 <?php
 require __DIR__ . '/functions.php';
+
+if (\Can\Has\who() !== null) {
+	\Can\Has\redirectToBase();
+}
 ?>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
