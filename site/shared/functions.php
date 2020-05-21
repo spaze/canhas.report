4399460b7ae601d841b2564c7af9c4ded1eb69a6
Michal Špaček <mail@michalspacek.cz>
2020-05-21 02:42:59 +0200

    Add favicons

--- site/shared/functions.php
+++ site/shared/functions.php
@@ -25,6 +25,8 @@ function pageHead(?string $title = null): string
 	return '<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>' . ($title ? " {$title} | " : '') . 'Reporting API Demos</title>
+		<link rel="icon" type="image/svg+xml" href="' . \htmlspecialchars(baseOrigin()) . '/assets/favicon.svg">
+		<link rel="alternate icon" href="' . \htmlspecialchars(baseOrigin()) . '/assets/favicon.png">
 		<link rel="stylesheet" href="' . \htmlspecialchars(baseOrigin()) . '/assets/style.css">
 		<script src="' . \htmlspecialchars(baseOrigin()) . '/assets/scripts.js"></script>
 		<script src="' . \htmlspecialchars(baseOrigin()) . '/assets/highlight.pack.js"></script>
