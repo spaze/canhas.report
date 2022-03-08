a7d18f6e97af7e1b77638995927e0ccc38643887
Michal Špaček <mail@michalspacek.cz>
2022-03-08 03:22:28 +0100

    Add Report-To header even when no reports are sent for 3rd party frames
    ...to actually demonstrate the fact.

--- site/pages/permissions-policy-iframes.php
+++ site/pages/permissions-policy-iframes.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types = 1);
 
+$reportToHeader = \Can\Has\reportToHeader();
 $permissionsPolicyHeader = 'Permissions-Policy: fullscreen=(self "https://exploited.cz")';
-$iframeUrl = 'https://exploited.cz/frames/fullscreen/fullscreen.html';
+header($reportToHeader);
 header($permissionsPolicyHeader);
+$iframeUrl = 'https://exploited.cz/frames/fullscreen/fullscreen.html';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,6 +24,7 @@ header($permissionsPolicyHeader);
 		That means for example that no iframe embedded in your page can go fullscreen, unless explicitly enabled, if your page has disallowed going fullscreen.
 	</em></p>
 	<?= \Can\Has\permissionsPolicyNotSupportedHtml('but <code>fullscreen</code> on <code>&lt;iframe&gt;</code> is partially supported in Firefox'); ?>
+	<?= \Can\Has\reportingApiNotSupportedHtml('but no reports would be sent from this page anyway'); ?>
 	<h2>The <code>Permissions-Policy</code> response header:</h2>
 	<pre><code class="json"><?= \Can\Has\highlight($permissionsPolicyHeader); ?></code></pre>
 	<ul>
@@ -34,13 +37,16 @@ header($permissionsPolicyHeader);
 		</li>
 	</ul>
 
+	<?= \Can\Has\reportToHeaderHtml($reportToHeader, 'the Permissions Policy reports will always be sent to the group named <code>default</code>'); ?>
+	<p><em>Note: the <code>Report-To</code> header here is mostly useless as the following violations happen in 3<sup>rd</sup> party embedded iframes, and no reports are sent in such cases.</em></p>
+
 	<h2>Embedded frame cannot go fullscreen</h2>
 	<?php \Can\Has\scriptSourceHtmlStart('blocked'); ?>
 	<iframe src="<?= htmlspecialchars($iframeUrl) ?>"></iframe>
 	<?php \Can\Has\scriptSourceHtmlEnd(); ?>
 	<ul>
 		<li>Fullscreen <span class="blocked">blocked</span> by the current <code>fullscreen</code> policy</li>
-		<li>No report will be sent, because the violation happened in an embedded iframe, and only first-party reports are sent</li>
+		<li><strong>No report will be sent</strong>, because the violation happened in an embedded iframe, and only first-party reports are sent</li>
 		<li>Violation will be visible in Developer Tools in the <em>Console</em> tab</li>
 	</ul>
 
