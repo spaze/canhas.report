8864a4d97b544b42916dd9dc372650d486df8ca1
Michal Špaček <mail@michalspacek.cz>
2025-02-11 16:45:16 +0100

    Allow inline critical CSS for report pages as well
    Regression introduced in #31

--- site/hivemind/index.php
+++ site/hivemind/index.php
@@ -5,7 +5,8 @@ require __DIR__ . '/../reports/config.php';
 require __DIR__ . '/../shared/functions.php';
 
 $baseOrigin = \Can\Has\baseOrigin();
-header("Content-Security-Policy: default-src 'none'; script-src {$baseOrigin}; img-src {$baseOrigin}; style-src {$baseOrigin}; base-uri 'none'; form-action 'none'");
+$nonce = \Can\Has\randomNonce();
+header("Content-Security-Policy: default-src 'none'; script-src {$baseOrigin}; img-src {$baseOrigin}; style-src {$baseOrigin} 'nonce-{$nonce}'; base-uri 'none'; form-action 'none'");
 
 $database = new PDO("mysql:host=$dbHost;dbname=$dbSchema", $dbUsername, $dbPassword);
 $statement = $database->prepare('DELETE FROM reports WHERE received < ?')->execute([(new DateTime('-' . \Can\Has\dataRetentionDays() . ' days'))->format('Y-m-d H:i:s')]);
@@ -20,7 +21,7 @@ if ($seen) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?= \Can\Has\pageHead('All Received Reports'); ?>
+<?= \Can\Has\pageHead('All Received Reports', $nonce); ?>
 <body>
 <?= \Can\Has\headerHtml('Browser Reporting Demos'); ?>
 <div id="main">
